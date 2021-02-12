2ff786682ec96b25b156cb2cc2fa42a68345b68e
Soundwood <j.benhogan@gmail.com>
2021-02-11 22:07:01 -0600

    title change

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
             <header className="blog-header py-3">
                 <div className="row flex-nowrap justify-content-between align-items-center">
                     <div className="col-4 pt-1"><a className="text-muted" href="#subscribe">Subscribe to Alerts</a></div>
-                    <div className="col-4 text-center"><a className="blog-header-logo text-dark" href="#home">Stonks Alert</a></div>
+                    <div className="col-4 text-center"><a className="blog-header-logo text-dark" href="#home">Stonk Trend Bot</a></div>
                     <div className="col-4 d-flex justify-content-end align-items-center"></div>
                 </div>
             </header>
